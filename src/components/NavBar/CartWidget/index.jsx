3c76c0158240f3447ed637fab7e1dd9845ed1189
FerNegruzzi <fernegruzzi12@gmail.com>
2022-12-19 14:51:38 -0300

    add navbar with cart and add itemlistcontainer

---
+++ src/components/NavBar/CartWidget/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HiShoppingCart } from "react-icons/hi";
+
+const CartWidget = () => {
+  return (
+    <HiShoppingCart size={'2em'}/>
+  )
+}
+
+export default CartWidget
---
+++ src/containers/ItemListContainer/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Item from '../../components/NavBar/Item';
+import './style.scss'
+
+const ItemListContainer = () => {
+    return (
+        <div className='item-list-container'>
+            <Item title={'Buzo Thrasher negro sin capucha'}/>
+            <Item title={'Buzo Thrasher negro sin capucha'}/>
+            <Item title={'Buzo Thrasher negro sin capucha'}/>
+            <Item title={'Buzo Thrasher negro sin capucha'}/>
+            <Item title={'Buzo Thrasher negro sin capucha'}/>
+            <Item title={'Buzo Thrasher negro sin capucha'}/>
+            <Item title={'Buzo Thrasher negro sin capucha'}/>
+        </div>
+    )
+}
+
+export default ItemListContainer
